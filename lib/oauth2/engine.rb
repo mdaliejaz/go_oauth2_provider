3c3731dfe91d5281bcc2c6d3315eeb1a0758decf
Ali <mdaliejaz@gmail.com>
2014-07-17 12:46:19 +0530

    #130 - oauth/engine.rb - starting point of rails engine. * Previously all the startup setup was done through init.rb, which is loaded on startup. * Code from old oauth2_provider init.rb is moved to lib/oauth2/engine.rb. This is a rails engine's starting point.

--- lib/oauth2/engine.rb
+++ lib/oauth2/engine.rb
@@ -1,4 +1,9 @@
+# Copyright (c) 2010 ThoughtWorks Inc. (http://thoughtworks.com)
+# Licenced under the MIT License (http://www.opensource.org/licenses/mit-license.php)
+
+# !!perform any initialization in oauth2_provider!!
 module Oauth2Provider
   class Engine < ::Rails::Engine
+    require 'oauth2_provider'
   end
 end
